c8f41502bf2e18d71ba9473dad1521433f5a090c
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-05-27 12:57:46 -0400

    Update Article.java and themes.xml
    Minor reformatting

--- app/src/main/java/com/example/buildmypc/ui/newsfeed/Article.java
+++ app/src/main/java/com/example/buildmypc/ui/newsfeed/Article.java
@@ -1,6 +1,5 @@
 package com.example.buildmypc.ui.newsfeed;
 
-import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 
 import java.net.URL;
@@ -14,11 +13,11 @@ public class Article {
 	private Drawable image;
 	private Date date;
 
-	public Article(){
+	public Article() {
 		this("", "", null, "", null, null);
 	}
 
-	public Article(String heading, String desc, URL originURL, String publisher, Drawable image, Date date){
+	public Article(String heading, String desc, URL originURL, String publisher, Drawable image, Date date) {
 		this.heading = heading;
 		this.desc = desc;
 		this.originURL = originURL;
@@ -27,16 +26,51 @@ public class Article {
 		this.date = date;
 	}
 
-	public String getHeading() { return heading; }
-	public void setHeading(String heading) { this.heading = heading; }
-	public String getDesc() { return desc; }
-	public void setDesc(String desc) { this.desc = desc; }
-	public URL getOriginURL() { return originURL; }
-	public void setOriginURL(URL originURL) { this.originURL = originURL; }
-	public String getPublisher() { return publisher; }
-	public void setPublisher(String publisher) { this.publisher = publisher; }
-	public Drawable getImage() { return image; }
-	public void setImage(Drawable image) { this.image = image; }
-	public Date getDate() {return date;}
-	public void setDate(Date date) { this.date = date; }
+	public String getHeading() {
+		return heading;
+	}
+
+	public void setHeading(String heading) {
+		this.heading = heading;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public URL getOriginURL() {
+		return originURL;
+	}
+
+	public void setOriginURL(URL originURL) {
+		this.originURL = originURL;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public Drawable getImage() {
+		return image;
+	}
+
+	public void setImage(Drawable image) {
+		this.image = image;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
