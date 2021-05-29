46c1cdbf7755167d39d80968ba4b586370cef0fd
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-05-29 17:29:14 -0400

    Update NewsfeedFragment.java

--- app/src/main/java/com/example/buildmypc/ui/newsfeed/NewsfeedFragment.java
+++ app/src/main/java/com/example/buildmypc/ui/newsfeed/NewsfeedFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,7 +16,6 @@ import static com.example.buildmypc.databinding.FragmentNewsfeedBinding.inflate;
 
 public class NewsfeedFragment extends Fragment {
 	private FragmentNewsfeedBinding binding;
-	ArrayAdapter<Article> articleArrayAdapter = new ArrayAdapter<Article>();
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		NewsfeedViewModel newsfeedViewModel = new ViewModelProvider(this).get(NewsfeedViewModel.class);
 		binding = inflate(inflater, container, false);
