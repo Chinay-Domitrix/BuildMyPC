be3b3b57c0742fcd2081da43dea5dfce84dfdb05
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-05-29 18:48:34 -0400

    Update RSSAsyncTask.java

--- app/src/main/java/com/example/buildmypc/ui/newsfeed/RSSAsyncTask.java
+++ app/src/main/java/com/example/buildmypc/ui/newsfeed/RSSAsyncTask.java
@@ -9,13 +9,9 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.net.URLConnection;
 import java.util.ArrayList;
-
-import okhttp3.OkHttpClient;
 
 public class RSSAsyncTask extends AsyncTask<String, Void, ArrayList<Article>> {
 	@Override
@@ -42,7 +38,7 @@ public class RSSAsyncTask extends AsyncTask<String, Void, ArrayList<Article>> {
 		}
 
 		// parsing the XML result into something workable
-		switch(publisher.toUpperCase()){
+		switch (publisher.toUpperCase()) {
 			case "TECHMEME":
 				break;
 
