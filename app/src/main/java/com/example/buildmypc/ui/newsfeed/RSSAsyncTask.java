9b4341a55948aa3d123ec15a422f6bfa094e8b9a
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-05-29 17:19:51 -0400

    Update RSSAsyncTask.java

--- app/src/main/java/com/example/buildmypc/ui/newsfeed/RSSAsyncTask.java
+++ app/src/main/java/com/example/buildmypc/ui/newsfeed/RSSAsyncTask.java
@@ -2,7 +2,7 @@ package com.example.buildmypc.ui.newsfeed;
 
 import android.os.AsyncTask;
 
-public class RSSAsync extends AsyncTask {
+public class RSSAsyncTask extends AsyncTask {
 	@Override
 	protected Object doInBackground(Object[] objects) {
 		return null;
