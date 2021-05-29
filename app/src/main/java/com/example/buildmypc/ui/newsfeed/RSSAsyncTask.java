fe176cb6761215a0ab2e39ce0d4e20c475776040
alpiemoodles <alktatstudios@gmail.com>
2021-05-29 17:41:36 -0400

    Written part of RSSAsyncTask to actually download stuff off of the internet; also changed the return type to Article for ease in later programming

--- app/src/main/java/com/example/buildmypc/ui/newsfeed/RSSAsyncTask.java
+++ app/src/main/java/com/example/buildmypc/ui/newsfeed/RSSAsyncTask.java
@@ -1,12 +1,49 @@
 package com.example.buildmypc.ui.newsfeed;
 
 import android.os.AsyncTask;
+import android.util.Log;
+import android.util.Xml;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLConnection;
+
+import okhttp3.OkHttpClient;
+
 public class RSSAsyncTask extends AsyncTask<String, Void, JSONObject> {
 	@Override
-	protected JSONObject doInBackground(String... strings) {
+	protected Article doInBackground(String... urlStrs) {
+		// building the string
+		URLConnection connection;
+
+		StringBuilder builder = new StringBuilder();
+		Xml xmlObject = null;
+		JSONObject webpageObj = null;
+		try {
+			URL url = new URL(urlStrs[0])
+			connection = url.openConnection();
+			InputStream stream = connection.getInputStream();
+			String line;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+			while((line = reader.readLine()) != null) {
+				builder.append(line);
+			}
+
+
+		} catch (Exception e){
+			Log.d("GAMING", "Stacking! + " + e.toString());
+		}
+
+
+
+
+
 		return null;
 	}
 }
