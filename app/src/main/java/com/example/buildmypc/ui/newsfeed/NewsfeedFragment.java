22f74228891828f9d40246ebd242f1cd731e13d8
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-05-29 17:19:18 -0400

    Transition to AsyncTask

--- app/src/main/java/com/example/buildmypc/ui/newsfeed/NewsfeedFragment.java
+++ app/src/main/java/com/example/buildmypc/ui/newsfeed/NewsfeedFragment.java
@@ -12,20 +12,6 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.buildmypc.databinding.FragmentNewsfeedBinding;
 
-import org.xml.sax.ContentHandler;
-import org.xml.sax.DTDHandler;
-import org.xml.sax.EntityResolver;
-import org.xml.sax.ErrorHandler;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXNotRecognizedException;
-import org.xml.sax.SAXNotSupportedException;
-import org.xml.sax.XMLReader;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-
 import static com.example.buildmypc.databinding.FragmentNewsfeedBinding.inflate;
 
 public class NewsfeedFragment extends Fragment {
@@ -38,81 +24,7 @@ public class NewsfeedFragment extends Fragment {
 		final TextView textView = binding.textSlideshow;
 		newsfeedViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 		// TODO parsing newsfeeds
-		try {
-			new XMLReader() {
-				@Override
-				public boolean getFeature(String name) throws SAXNotRecognizedException, SAXNotSupportedException {
-					return false;
-				}
 
-				@Override
-				public void setFeature(String name, boolean value) throws SAXNotRecognizedException, SAXNotSupportedException {
-
-				}
-
-				@Override
-				public Object getProperty(String name) throws SAXNotRecognizedException, SAXNotSupportedException {
-					return null;
-				}
-
-				@Override
-				public void setProperty(String name, Object value) throws SAXNotRecognizedException, SAXNotSupportedException {
-
-				}
-
-				@Override
-				public void setEntityResolver(EntityResolver resolver) {
-
-				}
-
-				@Override
-				public EntityResolver getEntityResolver() {
-					return null;
-				}
-
-				@Override
-				public void setDTDHandler(DTDHandler handler) {
-
-				}
-
-				@Override
-				public DTDHandler getDTDHandler() {
-					return null;
-				}
-
-				@Override
-				public void setContentHandler(ContentHandler handler) {
-
-				}
-
-				@Override
-				public ContentHandler getContentHandler() {
-					return null;
-				}
-
-				@Override
-				public void setErrorHandler(ErrorHandler handler) {
-
-				}
-
-				@Override
-				public ErrorHandler getErrorHandler() {
-					return null;
-				}
-
-				@Override
-				public void parse(InputSource input) throws IOException, SAXException {
-
-				}
-
-				@Override
-				public void parse(String systemId) throws IOException, SAXException {
-
-				}
-			}.parse(new URL("https://www.engadget.com/rss.xml").toURI().toString());
-		} catch (IOException | SAXException | URISyntaxException e) {
-			e.printStackTrace();
-		}
 		// ending return
 		return root;
 	}
---
+++ app/src/main/java/com/example/buildmypc/ui/newsfeed/RSSAsync.java
@@ -0,0 +1,10 @@
+package com.example.buildmypc.ui.newsfeed;
+
+import android.os.AsyncTask;
+
+public class RSSAsync extends AsyncTask {
+	@Override
+	protected Object doInBackground(Object[] objects) {
+		return null;
+	}
+}
