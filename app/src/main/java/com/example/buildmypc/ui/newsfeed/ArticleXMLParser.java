bf2ee053dd53ec16fa39c48e6df2ab542ec0da94
alpiemoodles <alktatstudios@gmail.com>
2021-05-29 18:43:12 -0400

    removed dependency on XMLToJson & wrote more of the parser

--- app/src/main/java/com/example/buildmypc/ui/newsfeed/ArticleXMLParser.java
+++ app/src/main/java/com/example/buildmypc/ui/newsfeed/ArticleXMLParser.java
@@ -1,5 +1,6 @@
 package com.example.buildmypc.ui.newsfeed;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ArticleXMLParser {
@@ -11,8 +12,9 @@ public class ArticleXMLParser {
 	private String descInd;
 	private String dateInd;
 	private String urlInd;
+	private String imageInd;
 
-	public ArticleXMLParser(String data, String publisher, String itemDivisor, String nameInd, String descInd, String dateInd, String urlInd) {
+	public ArticleXMLParser(String data, String publisher, String itemDivisor, String nameInd, String descInd, String dateInd, String urlInd, String imageInd) {
 		this.data = data;
 		this.publisher = publisher.toUpperCase();
 		this.itemDivisor = itemDivisor.toUpperCase();
@@ -20,15 +22,35 @@ public class ArticleXMLParser {
 		this.descInd = descInd.toUpperCase();
 		this.dateInd = dateInd.toUpperCase();
 		this.urlInd = urlInd;
+		this.imageInd = imageInd;
 	}
 
 	public ArrayList<Article> parseData(){
+		ArrayList<Article> articles = new ArrayList<Article>();
+		Article currentArticle = new Article();
 		String[] dataArr = data.split("r'[\\r\\n]+'");
-		int index = 0;
+		int dataIndex = 0;
 		boolean contLoop = true;
-		while(contLoop && index < dataArr.length){
-			String line = dataArr[index];
-			if()
+		while(contLoop && dataIndex < dataArr.length){
+			String line = dataArr[dataIndex];
+			if(line.contains("<" + itemDivisor)){ // indicator that divides up different elements
+				articles.add(new Article());
+			}
+			else if(line.contains("<" + nameInd)){ // name indicator code
+				currentArticle = articles.get(articles.size() - 1);
+				currentArticle.setHeading();
+			}
+			else if(line.contains("<" + descInd)){ //TODO determine whether this is able to even be shown
+
+			}
+			else if(line.contains("<" + dateInd)){ // date indicator code
+
+			}
+			else if(line.contains("<" + urlInd)){ // url indicator
+
+			}
+
+			dataIndex++;
 		}
 
 	}
