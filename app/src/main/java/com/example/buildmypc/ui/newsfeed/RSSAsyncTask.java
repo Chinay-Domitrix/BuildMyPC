c8569b049ba360028c8902cc1bbba172191f2e47
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-06-03 11:37:51 -0400

    Update RSSAsyncTask.java

--- app/src/main/java/com/example/buildmypc/ui/newsfeed/RSSAsyncTask.java
+++ app/src/main/java/com/example/buildmypc/ui/newsfeed/RSSAsyncTask.java
@@ -6,46 +6,41 @@ import android.util.Xml;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.text.ParseException;
 import java.util.ArrayList;
 
 import static android.util.Log.d;
 import static com.example.buildmypc.ui.newsfeed.NewsfeedFragment.finalArticleList;
 
-public class RSSAsyncTask extends AsyncTask<String, Void, ArrayList<Article>> {
+class RSSAsyncTask extends AsyncTask<String, Void, ArrayList<Article>> {
 	@Override
 	protected ArrayList<Article> doInBackground(String... strings) {
-		assert strings != null && strings.length > 0;
+		assert (strings != null) && (strings.length > 0);
 		ArrayList<Article> usableArticleList = new ArrayList<>();
-		URL url;
-		String publisher = strings[1];
 		// building a string by downloading off of le interwebz (c&p from WeatherApp)
-		URLConnection connection;
 		StringBuilder builder = new StringBuilder();
 		Xml xmlObject = null;
 		ArticleXMLParser xmlParser;
 		JSONObject webpageObj = null;
 		try {
-			url = new URL(strings[0]);
-			connection = url.openConnection();
-			InputStream stream = connection.getInputStream();
 			String line;
-			BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(strings[0]).openConnection().getInputStream()));
 			while ((line = reader.readLine()) != null)
 				builder.append(line); // the total file, once this runs, is converted into a string and is accessible via "builder.toString();"
-		} catch (Exception e) {
+		} catch (IOException e) {
 			d("GAMING", "Stacking! + " + e.toString());
 		}
 		// parsing the XML result into something workable
-		switch (publisher.toUpperCase()) {
+		switch (strings[1].toUpperCase()) {
 			case "TECHMEME":
 				xmlParser = new ArticleXMLParser(builder.toString(), "Techmeme", "item", "title", "description", "pubDate", "link", "N/A");
 				try { // try-catch handles both the URLMalformedException and the Parse-based-Exception
 					usableArticleList.addAll(xmlParser.parseData());
-				} catch (Exception e) {
+				} catch (ParseException | MalformedURLException e) {
 					d("CHIRAGERROR", e.toString());
 				}
 				break;
@@ -64,7 +59,7 @@ public class RSSAsyncTask extends AsyncTask<String, Void, ArrayList<Article>> {
 //					for(Article article : xmlParser.parseData()){
 //						usableArticleList.add(article);
 //					}
-//				} catch (Exception e) {
+//				} catch (ParseException | MalformedURLException e) {
 //					Log.d("CHIRAGERROR", e.toString());
 //				}
 //				break;
@@ -83,7 +78,7 @@ public class RSSAsyncTask extends AsyncTask<String, Void, ArrayList<Article>> {
 //					for(Article article : xmlParser.parseData()){
 //						usableArticleList.add(article);
 //					}
-//				} catch (Exception e) {
+//				} catch (ParseException | MalformedURLException e) {
 //					Log.d("CHIRAGERROR", e.toString());
 //				}
 //				break;
@@ -91,7 +86,7 @@ public class RSSAsyncTask extends AsyncTask<String, Void, ArrayList<Article>> {
 				xmlParser = new ArticleXMLParser(builder.toString(), "The Vergecast", "entry", "title", "content", "published", "id", "N/A");
 				try { // try-catch handles both the URLMalformedException and the Parse-based-Exception
 					usableArticleList.addAll(xmlParser.parseData());
-				} catch (Exception e) {
+				} catch (ParseException | MalformedURLException e) {
 					d("CHIRAGERROR", e.toString());
 				}
 				break;
@@ -99,7 +94,7 @@ public class RSSAsyncTask extends AsyncTask<String, Void, ArrayList<Article>> {
 				xmlParser = new ArticleXMLParser(builder.toString(), "WIRED", "item", "title", "description", "pubDate", "link", "media:thumbnail");
 				try { // try-catch handles both the URLMalformedException and the Parse-based-Exception
 					usableArticleList.addAll(xmlParser.parseData());
-				} catch (Exception e) {
+				} catch (ParseException | MalformedURLException e) {
 					d("CHIRAGERROR", e.toString());
 				}
 				break;
@@ -107,7 +102,7 @@ public class RSSAsyncTask extends AsyncTask<String, Void, ArrayList<Article>> {
 				xmlParser = new ArticleXMLParser(builder.toString(), "NYT Technology", "item", "title", "description", "pubDate", "link", "media:content");
 				try { // try-catch handles both the URLMalformedException and the Parse-based-Exception
 					usableArticleList.addAll(xmlParser.parseData());
-				} catch (Exception e) {
+				} catch (ParseException | MalformedURLException e) {
 					d("CHIRAGERROR", e.toString());
 				}
 				break;
@@ -117,7 +112,7 @@ public class RSSAsyncTask extends AsyncTask<String, Void, ArrayList<Article>> {
 				xmlParser = new ArticleXMLParser(builder.toString(), "Engadget", "item", "title", "description", "pubDate", "link", "media:content");
 				try { // try-catch handles both the URLMalformedException and the Parse-based-Exception
 					usableArticleList.addAll(xmlParser.parseData());
-				} catch (Exception e) {
+				} catch (ParseException | MalformedURLException e) {
 					d("CHIRAGERROR", e.toString());
 				}
 				break;
