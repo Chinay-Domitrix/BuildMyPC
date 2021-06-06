e13cb28bbdfa73130eb5d6f935563d33e7ad32d3
alpiemoodles <alktatstudios@gmail.com>
2021-06-05 20:38:19 -0400

    UPDATE - absolutely gutted the GridView in favor of a RecyclerView which I actually half understand and IT WORKS - added RecyclerView that accurately displays everything in the exact way I want it to - poggies

--- app/src/main/java/com/example/buildmypc/ui/build/BuildFragment.java
+++ app/src/main/java/com/example/buildmypc/ui/build/BuildFragment.java
@@ -1,7 +1,9 @@
 package com.example.buildmypc.ui.build;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,8 @@ import androidx.annotation.NonNull;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.buildmypc.R;
 import com.example.buildmypc.databinding.FragmentHomeBinding;
@@ -28,6 +32,8 @@ import com.example.buildmypc.ui.parts.parts.OS;
 import com.example.buildmypc.ui.parts.parts.PSU;
 import com.example.buildmypc.ui.parts.parts.Part;
 import com.example.buildmypc.ui.parts.parts.StorageComp;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -43,15 +49,20 @@ public class BuildFragment extends Fragment {
 		binding = inflate(inflater, container, false);
 		View root = binding.getRoot();
 //		final TextView textView = binding.textHome;
-		GridView gridView = binding.buildfragGridView;
 //		buildViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
 		// building myBuilds
 		displayedBuilds = new ArrayList<PCBuild>();
-//		Prebuilds prebuilds = new Prebuilds();
 		displayedBuilds.addAll(new Prebuilds().getPrebuiltList());
-		GridAdapter adapter = new GridAdapter(getActivity(), displayedBuilds);
-		gridView.setOnItemClickListener((parent, view, position, id) -> Toast.makeText(getContext(), "activated " + position, Toast.LENGTH_SHORT).show());
+		// TODO add adding custom builds, saving them, and accessing them in the future (prob Sunday/Monday/Tuesday if we're that late)
+
+		RecyclerView recyclerView = binding.buildFragRecyclerView;
+		RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), 3);
+		recyclerView.setHasFixedSize(true);
+		recyclerView.setLayoutManager(layoutManager);
+		GridAdapter gridAdapter = new GridAdapter(getContext(), displayedBuilds);
+		recyclerView.setAdapter(gridAdapter);
+
 		return root;
 	}
 
@@ -61,47 +72,50 @@ public class BuildFragment extends Fragment {
 		binding = null;
 	}
 
-	private class GridAdapter extends BaseAdapter {
-		private final Context context;
-		private LayoutInflater inflater;
-		private final ArrayList<PCBuild> list;
+	private class GridAdapter extends RecyclerView.Adapter<GridAdapter.GridHolder> {
 
-		public GridAdapter(Context c, ArrayList<PCBuild> l) {
-			context = c;
-			list = l;
+		private Context context;
+		private ArrayList<PCBuild> buildList;
+
+		public GridAdapter(Context context, ArrayList<PCBuild> buildList) {
+			this.context = context;
+			this.buildList = buildList;
+		}
+
+
+		@NonNull
+		@NotNull
+		@Override
+		public GridHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+			View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item, null);
+			GridHolder holder = new GridHolder(layout);
+
+			return holder;
 		}
 
 		@Override
-		public int getCount() {
-			return list.size();
+		public void onBindViewHolder(@NonNull @NotNull GridHolder holder, int position) {
+			PCBuild currentBuild = buildList.get(position);
+
+			holder.image.setImageDrawable(currentBuild.getLogo());
+			holder.text.setText(currentBuild.getName());
 		}
 
 		@Override
-		public Object getItem(int position) {
-			return list.get(position);
+		public int getItemCount() {
+			return buildList.size();
 		}
 
-		@Override
-		public long getItemId(int position) {
-			return 0;
-		}
+		public class GridHolder extends RecyclerView.ViewHolder{
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			if (inflater == null)
-				inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			if (convertView == null) convertView = inflater.inflate(R.layout.row_item, null);
-			// do stuff here
+			ImageView image;
+			TextView text;
 
-			PCBuild currentBuild = displayedBuilds.get(position);
-
-			ImageView imageView = convertView.findViewById(R.id.gridView_imageView);
-			TextView titleTextView = convertView.findViewById(R.id.gridView_textView);
-
-			imageView.setImageDrawable(currentBuild.getLogo());
-			titleTextView.setText(currentBuild.getName());
-
-			return convertView;
+			public GridHolder(@NonNull @NotNull View itemView) {
+				super(itemView);
+				image = itemView.findViewById(R.id.gridView_imageView);
+				text = itemView.findViewById(R.id.gridView_textView);
+			}
 		}
 	}
 
