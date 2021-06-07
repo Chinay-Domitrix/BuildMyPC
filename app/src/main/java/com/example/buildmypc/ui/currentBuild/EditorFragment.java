7327cd84e091d250abbef71ebe0d284836684b55
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-06-07 08:02:24 -0400

    Update EditorFragment.java

--- app/src/main/java/com/example/buildmypc/ui/currentBuild/EditorFragment.java
+++ app/src/main/java/com/example/buildmypc/ui/currentBuild/EditorFragment.java
@@ -43,7 +43,7 @@ public class EditorFragment extends Fragment {
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
 	                         @Nullable Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.editor_fragment, container, false);
+		return inflater.inflate(R.layout.fragment_editor, container, false);
 	}
 
 	@Override
