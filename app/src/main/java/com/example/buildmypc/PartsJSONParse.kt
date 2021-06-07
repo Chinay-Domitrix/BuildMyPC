129db31e68c85184be30a6702559106f5c9dca58
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-06-06 23:05:21 -0400

    Create PartsJSONParse.kt

---
+++ app/src/main/java/com/example/buildmypc/PartsJSONParse.kt
@@ -0,0 +1,36 @@
+package com.example.buildmypc
+
+import com.example.buildmypc.MainActivity.parts
+import com.example.buildmypc.ui.parts.parts.CPU
+
+class PartsJSONParse : Runnable {
+	override fun run() {
+		val tempJSONObject = parts.get()
+		val cpus = tempJSONObject.getJSONArray("cpus")
+		val tempCPUS = MainActivity.cpus.get()
+		(0 until cpus.length()).forEach {
+			cpus.getJSONObject(it).apply {
+				tempCPUS.add(
+					CPU(
+						getString("model"),
+						getString("manufacturer"),
+						getInt("core-count"),
+						getDouble("core-clock-ghz"),
+						getDouble("boost-clock-ghz"),
+						getInt("tdp-w"),
+						getString("series"),
+						getString("microarchitecture"),
+						getString("core-family"),
+						getString("socket"),
+						getBoolean("integrated-gpu"),
+						getInt("max-memory-support-gb"),
+						getBoolean("ecc"),
+						getBoolean("cooler"),
+						getBoolean("smt")
+					)
+				)
+			}
+		}
+		MainActivity.cpus.set(tempCPUS)
+	}
+}
