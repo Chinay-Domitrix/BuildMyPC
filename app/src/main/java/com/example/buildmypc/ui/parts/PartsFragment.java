fd21c31b785e6ba6394ffa3e31732686bb94636d
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-06-07 12:19:12 -0400

    Update PartsFragment.java

--- app/src/main/java/com/example/buildmypc/ui/parts/PartsFragment.java
+++ app/src/main/java/com/example/buildmypc/ui/parts/PartsFragment.java
@@ -9,43 +9,47 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.toolbox.StringRequest;
+import com.example.buildmypc.PartsJSONParse;
 import com.example.buildmypc.databinding.FragmentPartsBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import static android.content.Context.CONNECTIVITY_SERVICE;
+import static com.android.volley.Request.Method.GET;
+import static com.android.volley.toolbox.Volley.newRequestQueue;
+import static com.example.buildmypc.MainActivity.parts;
+import static com.example.buildmypc.R.string.firebase_key;
+import static com.example.buildmypc.R.string.parts_list;
 import static com.example.buildmypc.databinding.FragmentPartsBinding.inflate;
 
 public class PartsFragment extends Fragment {
 	private FragmentPartsBinding binding;
 
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		PartsViewModel partsViewModel = new ViewModelProvider(this).get(PartsViewModel.class);
 		binding = inflate(inflater, container, false);
 		View root = binding.getRoot();
-//		final TextView textView = binding.textParts;
 		ConnectivityManager cm = ((ConnectivityManager) root.getContext().getSystemService(CONNECTIVITY_SERVICE));
 		NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-		if (activeNetwork != null && activeNetwork.isConnected() && !cm.isActiveNetworkMetered()) {
-//			FirebaseDatabase firebaseDatabase = database.get();
-//			DatabaseReference cpu = firebaseDatabase.getReference("cpu"),
-//					cooler = firebaseDatabase.getReference("cooler"),
-//					motherboard = firebaseDatabase.getReference("motherboard"),
-//					memory = firebaseDatabase.getReference("memory"),
-//					storage = firebaseDatabase.getReference("storage"),
-//					gpu = firebaseDatabase.getReference("gpu"),
-//					pcCase = firebaseDatabase.getReference("case"),
-//					psu = firebaseDatabase.getReference("psu"),
-//					os = firebaseDatabase.getReference("os"),
-//					monitor = firebaseDatabase.getReference("monitor");
-			new Thread() {
-
-			}.start();
-		} else {
-
+		if ((activeNetwork != null) && activeNetwork.isConnected() && !cm.isActiveNetworkMetered())
+			newRequestQueue(root.getContext()).add(new StringRequest(GET,
+					"https://firebasestorage.googleapis.com/v0/b/buildmypc-ac8c3.appspot.com/o/part_data.json?alt=media&token="+getString(firebase_key),
+					response -> {
+						try {
+							parts.set(new JSONObject(response));
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+					},
+					Throwable::printStackTrace));
+		else try {
+			parts.set(new JSONObject(getString(parts_list)));
+		} catch (JSONException e) {
+			e.printStackTrace();
 		}
-
-//		partsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+		new PartsJSONParse().start();
 		return root;
 	}
 
