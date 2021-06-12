1a465b8793c0fc37cc2cf17f25a2369e2331ca14
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-06-11 23:16:44 -0400

    Update BuildFragment.java
    Redid Parcel constructor

--- app/src/main/java/com/example/buildmypc/ui/build/BuildFragment.java
+++ app/src/main/java/com/example/buildmypc/ui/build/BuildFragment.java
@@ -1,9 +1,7 @@
 package com.example.buildmypc.ui.build;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,18 +10,13 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentResultListener;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.LayoutManager;
 
-import com.example.buildmypc.R;
 import com.example.buildmypc.databinding.FragmentHomeBinding;
-import com.example.buildmypc.ui.currentBuild.Build;
 import com.example.buildmypc.ui.currentBuild.EditorFragment;
 import com.example.buildmypc.ui.parts.parts.CPU;
 import com.example.buildmypc.ui.parts.parts.Case;
@@ -34,38 +27,50 @@ import com.example.buildmypc.ui.parts.parts.Monitor;
 import com.example.buildmypc.ui.parts.parts.Motherboard;
 import com.example.buildmypc.ui.parts.parts.OS;
 import com.example.buildmypc.ui.parts.parts.PSU;
-import com.example.buildmypc.ui.parts.parts.Part;
 import com.example.buildmypc.ui.parts.parts.Storage;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
+import static android.view.LayoutInflater.from;
+import static androidx.core.content.res.ResourcesCompat.getDrawable;
+import static com.example.buildmypc.R.anim.enter_from_right;
+import static com.example.buildmypc.R.anim.exit_to_right;
+import static com.example.buildmypc.R.drawable.a_275r_black;
+import static com.example.buildmypc.R.drawable.a_4000d_airflow_black;
+import static com.example.buildmypc.R.drawable.h510_elite_black;
+import static com.example.buildmypc.R.drawable.h510_elite_white;
+import static com.example.buildmypc.R.id.gridView_imageView;
+import static com.example.buildmypc.R.id.gridView_textView;
+import static com.example.buildmypc.R.id.nav_host_fragment_content_main;
+import static com.example.buildmypc.R.layout.row_item;
 import static com.example.buildmypc.databinding.FragmentHomeBinding.inflate;
 
 public class BuildFragment extends Fragment {
-	ArrayList<PCBuild> displayedBuilds;
-	private FragmentHomeBinding binding;
-	PCBuild currentEditedBuild;
-
 	private static final String BUILD = "pcbuild";
+	ArrayList<PCBuild> displayedBuilds;
+	PCBuild currentEditedBuild;
+	private FragmentHomeBinding binding;
 
 	public BuildFragment(PCBuild build) {
 		this.currentEditedBuild = build;
 	}
 
-	public BuildFragment() {}
+	public BuildFragment() {
+	}
+
+	public static BuildFragment newInstance(PCBuild build) {
+		BuildFragment fragment = new BuildFragment();
+		Bundle args = new Bundle();
+		args.putParcelable(BUILD, build);
+		return new BuildFragment(build);
+	}
 
 	@Override
-	public void onCreate(@Nullable Bundle savedInstanceState){
+	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getParentFragmentManager().setFragmentResultListener(BUILD, this, new FragmentResultListener() {
-			@Override
-			public void onFragmentResult(@NonNull @NotNull String requestKey, @NonNull @NotNull Bundle result) {
-				currentEditedBuild = result.getParcelable(BUILD);
-			}
-		});
+		getParentFragmentManager().setFragmentResultListener(BUILD, this, (requestKey, result) -> currentEditedBuild = result.getParcelable(BUILD));
 	}
 
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -80,13 +85,13 @@ public class BuildFragment extends Fragment {
 //		buildViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
 		// building myBuilds
-		displayedBuilds = new ArrayList<PCBuild>();
+		displayedBuilds = new ArrayList<>();
 		displayedBuilds.addAll(new Prebuilds().getPrebuiltList());
-		if(currentEditedBuild != null) displayedBuilds.add(currentEditedBuild);
-		// TODO add adding custom builds, saving them, and accessing them in the future (prob Sunday/Monday/Tuesday if we're that late)
+		if (currentEditedBuild != null) displayedBuilds.add(currentEditedBuild);
+//		TODO add adding custom builds, saving them, and accessing them in the future (prob Sunday/Monday/Tuesday if we're that late)
 
 		RecyclerView recyclerView = binding.buildFragRecyclerView;
-		RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), 3);
+		LayoutManager layoutManager = new GridLayoutManager(getActivity(), 3);
 		recyclerView.setHasFixedSize(true);
 		recyclerView.setLayoutManager(layoutManager);
 		GridAdapter gridAdapter = new GridAdapter(getContext(), displayedBuilds);
@@ -101,13 +106,20 @@ public class BuildFragment extends Fragment {
 		binding = null;
 	}
 
+	private void openEditorFragment(PCBuild currentBuild) {
+		EditorFragment fragment = EditorFragment.newInstance(currentBuild);
+		getParentFragmentManager().beginTransaction()
+				.setCustomAnimations(enter_from_right, exit_to_right, enter_from_right, exit_to_right)
+				.addToBackStack(null)
+				.add(nav_host_fragment_content_main, fragment, BUILD)
+				.commit();
+	}
+
 	private class GridAdapter extends RecyclerView.Adapter<GridAdapter.GridHolder> {
 
-		private Context context;
-		private ArrayList<PCBuild> buildList;
+		private final ArrayList<PCBuild> buildList;
 
 		public GridAdapter(Context context, ArrayList<PCBuild> buildList) {
-			this.context = context;
 			this.buildList = buildList;
 		}
 
@@ -115,7 +127,7 @@ public class BuildFragment extends Fragment {
 		@NotNull
 		@Override
 		public GridHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-			View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item, null);
+			View layout = from(parent.getContext()).inflate(row_item, null);
 			GridHolder holder = new GridHolder(layout);
 
 			return holder;
@@ -128,17 +140,14 @@ public class BuildFragment extends Fragment {
 			holder.image.setImageDrawable(currentBuild.getLogo());
 			holder.text.setText(currentBuild.getName());
 			// setting the OnClickListener to start a new fragment while passing in the currentBuild object
-			holder.image.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					int id = ((ViewGroup) getView().getParent()).getId();
-					getActivity().getSupportFragmentManager().beginTransaction()
-							.replace(id, new EditorFragment(currentBuild), "findThisFragment")
-							.addToBackStack(null)
-							.commit();
+			holder.image.setOnClickListener(v -> {
+				int id = ((ViewGroup) requireView().getParent()).getId();
+				requireActivity().getSupportFragmentManager().beginTransaction()
+						.replace(id, new EditorFragment(currentBuild), "findThisFragment")
+						.addToBackStack(null)
+						.commit();
 //					openEditorFragment(currentBuild);
 
-				}
 			});
 		}
 
@@ -149,13 +158,13 @@ public class BuildFragment extends Fragment {
 
 		public class GridHolder extends RecyclerView.ViewHolder {
 
-			ImageView image;
-			TextView text;
+			final ImageView image;
+			final TextView text;
 
 			public GridHolder(@NonNull @NotNull View itemView) {
 				super(itemView);
-				image = itemView.findViewById(R.id.gridView_imageView);
-				text = itemView.findViewById(R.id.gridView_textView);
+				image = itemView.findViewById(gridView_imageView);
+				text = itemView.findViewById(gridView_textView);
 
 //				image.setOnClickListener(new View.OnClickListener() {
 //					@Override
@@ -167,34 +176,14 @@ public class BuildFragment extends Fragment {
 		}
 	}
 
-	private void openEditorFragment(PCBuild currentBuild) {
-		EditorFragment fragment = EditorFragment.newInstance(currentBuild);
-		getParentFragmentManager().beginTransaction()
-				.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right)
-				.addToBackStack(null)
-				.add(R.id.nav_host_fragment_content_main, fragment, BUILD)
-				.commit();
-	}
-
-	public static BuildFragment newInstance(PCBuild build) {
-		BuildFragment fragment = new BuildFragment();
-		Bundle args = new Bundle();
-		args.putParcelable(BUILD, build);
-		return new BuildFragment(build);
-	}
-
 	public class Prebuilds {
-		private ArrayList<PCBuild> builds;
-
-		public ArrayList<PCBuild> getPrebuiltList() {
-			return builds;
-		}
+		private final ArrayList<PCBuild> builds;
 
 		public Prebuilds() {
-			builds = new ArrayList<PCBuild>();
+			builds = new ArrayList<>();
 			builds.add(new PCBuild(
 					"Build 1",
-					ResourcesCompat.getDrawable(getResources(), R.drawable.h510_elite_black, getResources().newTheme()),
+					getDrawable(getResources(), h510_elite_black, getResources().newTheme()),
 					new Case("H510 Elite Tower", "Corsair", "Black"),
 					new Cooler("Hyper 212 EVO", "Cooler Master"),
 					new CPU("Ryzen 5 3600", "AMD"),
@@ -205,11 +194,11 @@ public class BuildFragment extends Fragment {
 					new OS("Windows 10 Pro", "Microsoft"),
 					new PSU("RM750 (2019)", "Corsair"),
 					new Storage("Barracuda Compute 2 TB", "Seagate"),
-					new ArrayList<Part>()
+					new ArrayList<>()
 			));
 			builds.add(new PCBuild(
 					"Build 2",
-					ResourcesCompat.getDrawable(getResources(), R.drawable.h510_elite_white, getResources().newTheme()),
+					getDrawable(getResources(), h510_elite_white, getResources().newTheme()),
 					new Case("H510 Elite Tower", "Corsair", "White"),
 					new Cooler("Hyper 212 EVO", "Cooler Master"),
 					new CPU("Ryzen 5 3600", "AMD"),
@@ -220,11 +209,11 @@ public class BuildFragment extends Fragment {
 					new OS("Windows 10 Pro", "Microsoft"),
 					new PSU("RM750 (2019)", "Corsair"),
 					new Storage("Barracuda Compute 2 TB", "Seagate"),
-					new ArrayList<Part>()
+					new ArrayList<>()
 			));
 			builds.add(new PCBuild(
 					"Build 3",
-					ResourcesCompat.getDrawable(getResources(), R.drawable.h510_elite_black, getResources().newTheme()),
+					getDrawable(getResources(), h510_elite_black, getResources().newTheme()),
 					new Case("H510 Elite Tower", "Corsair", "Black"),
 					new Cooler("Hyper 212 EVO", "Cooler Master"),
 					new CPU("Ryzen 5 3600", "AMD"),
@@ -235,11 +224,11 @@ public class BuildFragment extends Fragment {
 					new OS("Windows 10 Pro", "Microsoft"),
 					new PSU("RM750 (2019)", "Corsair"),
 					new Storage("Barracuda Compute 2 TB", "Seagate"),
-					new ArrayList<Part>()
+					new ArrayList<>()
 			));
 			builds.add(new PCBuild(
 					"Build 4",
-					ResourcesCompat.getDrawable(getResources(), R.drawable.a_275r_black, getResources().newTheme()),
+					getDrawable(getResources(), a_275r_black, getResources().newTheme()),
 					new Case("275R Tower", "Corsair", "Black"),
 					new Cooler("Hyper 212 EVO", "Cooler Master"),
 					new CPU("Ryzen 5 3600", "AMD"),
@@ -250,11 +239,11 @@ public class BuildFragment extends Fragment {
 					new OS("Windows 10 Pro", "Microsoft"),
 					new PSU("RM750 (2019)", "Corsair"),
 					new Storage("Barracuda Compute 2 TB", "Seagate"),
-					new ArrayList<Part>()
+					new ArrayList<>()
 			));
 			builds.add(new PCBuild(
 					"Build 5",
-					ResourcesCompat.getDrawable(getResources(), R.drawable.a_4000d_airflow_black, getResources().newTheme()),
+					getDrawable(getResources(), a_4000d_airflow_black, getResources().newTheme()),
 					new Case("4000D Airflow", "Corsair", "Black"),
 					new Cooler("Hyper 212 EVO", "Cooler Master"),
 					new CPU("Ryzen 5 3600", "AMD"),
@@ -265,11 +254,11 @@ public class BuildFragment extends Fragment {
 					new OS("Windows 10 Pro", "Microsoft"),
 					new PSU("RM750 (2019)", "Corsair"),
 					new Storage("Barracuda Compute 2 TB", "Seagate"),
-					new ArrayList<Part>()
+					new ArrayList<>()
 			));
 			builds.add(new PCBuild(
 					"Build 6",
-					ResourcesCompat.getDrawable(getResources(), R.drawable.h510_elite_black, getResources().newTheme()),
+					getDrawable(getResources(), h510_elite_black, getResources().newTheme()),
 					new Case("H510 Elite Tower", "Corsair", "Black"),
 					new Cooler("Hyper 212 EVO", "Cooler Master"),
 					new CPU("Ryzen 5 3600", "AMD"),
@@ -280,8 +269,12 @@ public class BuildFragment extends Fragment {
 					new OS("Windows 10 Pro", "Microsoft"),
 					new PSU("RM750 (2019)", "Corsair"),
 					new Storage("Barracuda Compute 2 TB", "Seagate"),
-					new ArrayList<Part>()
+					new ArrayList<>()
 			));
+		}
+
+		public ArrayList<PCBuild> getPrebuiltList() {
+			return builds;
 		}
 	}
 }
