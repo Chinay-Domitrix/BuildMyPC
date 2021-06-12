1757b4c04bc5b8bdfa96e958b550a54796559241
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-06-12 13:17:04 -0400

    Update PartsFragment.java

--- app/src/main/java/com/example/buildmypc/ui/parts/PartsFragment.java
+++ app/src/main/java/com/example/buildmypc/ui/parts/PartsFragment.java
@@ -1,8 +1,6 @@
 package com.example.buildmypc.ui.parts;
 
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,36 +14,26 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.Adapter;
 
-import com.android.volley.toolbox.StringRequest;
-import com.example.buildmypc.PartsJSONParse;
 import com.example.buildmypc.R;
 import com.example.buildmypc.databinding.FragmentPartsBinding;
 import com.example.buildmypc.ui.parts.parts.Part;
 
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-import static android.content.Context.CONNECTIVITY_SERVICE;
 import static android.view.LayoutInflater.from;
-import static com.android.volley.Request.Method.GET;
-import static com.android.volley.toolbox.Volley.newRequestQueue;
 import static com.example.buildmypc.MainActivity.coolers;
 import static com.example.buildmypc.MainActivity.cpus;
 import static com.example.buildmypc.MainActivity.gpus;
 import static com.example.buildmypc.MainActivity.monitors;
 import static com.example.buildmypc.MainActivity.motherboards;
 import static com.example.buildmypc.MainActivity.oss;
-import static com.example.buildmypc.MainActivity.parts;
 import static com.example.buildmypc.MainActivity.pcCases;
 import static com.example.buildmypc.MainActivity.psus;
 import static com.example.buildmypc.MainActivity.storage;
 import static com.example.buildmypc.R.id.partsList_addButton;
 import static com.example.buildmypc.R.id.partsList_nameTextView;
-import static com.example.buildmypc.R.string.firebase_key;
-import static com.example.buildmypc.R.string.parts_list;
 import static com.example.buildmypc.databinding.FragmentPartsBinding.inflate;
 
 public class PartsFragment extends Fragment {
@@ -54,27 +42,6 @@ public class PartsFragment extends Fragment {
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		binding = inflate(inflater, container, false);
 		View root = binding.getRoot();
-		if (parts.get().equals(new JSONObject())) {
-			ConnectivityManager cm = ((ConnectivityManager) root.getContext().getSystemService(CONNECTIVITY_SERVICE));
-			NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-			if ((activeNetwork != null) && activeNetwork.isConnected() && !cm.isActiveNetworkMetered())
-				newRequestQueue(root.getContext()).add(new StringRequest(GET,
-						"https://firebasestorage.googleapis.com/v0/b/buildmypc-ac8c3.appspot.com/o/part_data.json?alt=media&token=" + getString(firebase_key),
-						response -> {
-							try {
-								parts.set(new JSONObject(response));
-							} catch (JSONException e) {
-								e.printStackTrace();
-							}
-						},
-						Throwable::printStackTrace));
-			else try {
-				parts.set(new JSONObject(getString(parts_list)));
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-			new PartsJSONParse().start();
-		}
 //		new PartsJSONParse().start();
 
 		// RecyclerView time
@@ -110,11 +77,11 @@ public class PartsFragment extends Fragment {
 		psuRecyclerView.setAdapter(new PartsRecyclerViewAdapter(getContext(), psus.get()));
 		psuRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-		RecyclerView osRecyclerView = binding.partsOsRecyclerview; // power supplies
+		RecyclerView osRecyclerView = binding.partsOsRecyclerview;
 		osRecyclerView.setAdapter(new PartsRecyclerViewAdapter(getContext(), oss.get()));
 		osRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-		RecyclerView monitorRecyclerView = binding.partsMonitorRecyclerview; // power supplies
+		RecyclerView monitorRecyclerView = binding.partsMonitorRecyclerview;
 		monitorRecyclerView.setAdapter(new PartsRecyclerViewAdapter(getContext(), monitors.get()));
 		monitorRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
