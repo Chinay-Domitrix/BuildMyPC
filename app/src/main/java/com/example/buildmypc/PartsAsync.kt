3cd56ea69a931944f5cd64071091ab34062e7a2f
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-06-13 22:59:37 -0400

    Update PartsAsync.kt

--- app/src/main/java/com/example/buildmypc/PartsAsync.kt
+++ app/src/main/java/com/example/buildmypc/PartsAsync.kt
@@ -4,24 +4,40 @@ package com.example.buildmypc
 
 import android.Manifest.permission.INTERNET
 import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.os.AsyncTask
+import android.widget.Toast
 import com.example.buildmypc.MainActivity.parts
 import com.example.buildmypc.R.string.firebase_key
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.json.JSONException
 import org.json.JSONObject
 import java.net.URL
 import java.util.*
 
-class PartsAsync(private val mainActivity: MainActivity) : AsyncTask<Void, Void, String>() {
-	override fun onPreExecute() {
-		super.onPreExecute()
+class PartsAsync(private val mainActivity: MainActivity) {
+	fun run() {
+		CoroutineScope(Dispatchers.IO).launch {
+			onPreExecute()
+			try {
+				val maxDownloadSize = 18533
+			} catch (e: Exception) {
+				withContext(Dispatchers.Main) {
+					Toast.makeText(mainActivity, e.message, Toast.LENGTH_LONG).show()
+				}
+			}
+		}
+	}
+
+	fun onPreExecute() {
 		if (mainActivity.checkSelfPermission(INTERNET) != PERMISSION_GRANTED) mainActivity.requestPermissions(
 			arrayOf(INTERNET),
 			0
 		)
 	}
 
-	override fun doInBackground(vararg strings: Void): String {
+	fun doInBackground(vararg strings: Void): String {
 		val x = Scanner(
 			URL(
 				"https://firebasestorage.googleapis.com/v0/b/buildmypc-ac8c3.appspot.com/o/part_data.json?alt=media&token=" + mainActivity.getString(
@@ -34,8 +50,7 @@ class PartsAsync(private val mainActivity: MainActivity) : AsyncTask<Void, Void,
 		return y
 	}
 
-	override fun onPostExecute(s: String) {
-		super.onPostExecute(s)
+	fun onPostExecute(s: String) {
 		try {
 			parts.set(JSONObject(s))
 		} catch (e: JSONException) {
