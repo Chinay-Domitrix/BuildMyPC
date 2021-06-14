9ec09cd5b837142b92c480062dee8dba8fe57c40
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-06-13 22:21:12 -0400

    Update PartsAsync.kt

--- app/src/main/java/com/example/buildmypc/PartsAsync.kt
+++ app/src/main/java/com/example/buildmypc/PartsAsync.kt
@@ -10,6 +10,7 @@ import com.example.buildmypc.R.string.firebase_key
 import org.json.JSONException
 import org.json.JSONObject
 import java.net.URL
+import java.util.*
 
 class PartsAsync(private val mainActivity: MainActivity) : AsyncTask<Void, Void, String>() {
 	override fun onPreExecute() {
@@ -20,12 +21,18 @@ class PartsAsync(private val mainActivity: MainActivity) : AsyncTask<Void, Void,
 		)
 	}
 
-	override fun doInBackground(vararg strings: Void) =
-		URL(
-			"https://firebasestorage.googleapis.com/v0/b/buildmypc-ac8c3.appspot.com/o/part_data.json?alt=media&token=" + mainActivity.getString(
-				firebase_key
-			)
-		).readText()
+	override fun doInBackground(vararg strings: Void): String {
+		val x = Scanner(
+			URL(
+				"https://firebasestorage.googleapis.com/v0/b/buildmypc-ac8c3.appspot.com/o/part_data.json?alt=media&token=" + mainActivity.getString(
+					firebase_key
+				)
+			).openConnection().getInputStream()
+		)
+		var y = ""
+		while (x.hasNextLine()) y += x.nextLine()
+		return y
+	}
 
 	override fun onPostExecute(s: String) {
 		super.onPostExecute(s)
