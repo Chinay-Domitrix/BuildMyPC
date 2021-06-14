810e102f39190b90c3b2c2dda137650f80ce099f
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-06-14 14:04:48 -0400

    Update PartsAsync.kt

--- app/src/main/java/com/example/buildmypc/PartsAsync.kt
+++ app/src/main/java/com/example/buildmypc/PartsAsync.kt
@@ -4,57 +4,42 @@ package com.example.buildmypc
 
 import android.Manifest.permission.INTERNET
 import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.widget.Toast
+import android.net.Uri.fromFile
+import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.makeText
 import com.example.buildmypc.MainActivity.parts
-import com.example.buildmypc.R.string.firebase_key
+import com.example.buildmypc.MainActivity.storageReference
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
-import org.json.JSONException
 import org.json.JSONObject
-import java.net.URL
-import java.util.*
+import java.io.File
+import java.io.File.createTempFile
 
 class PartsAsync(private val mainActivity: MainActivity) {
 	fun run() {
-		CoroutineScope(Dispatchers.IO).launch {
+		CoroutineScope(IO).launch {
 			onPreExecute()
 			try {
-				val maxDownloadSize = 18533
+				storageReference.get().child("part_data.json")
+					.getFile(fromFile(withContext(IO) { createTempFile("part_data", ".json") }))
+					.await()
+				withContext(Main) { parts.set(JSONObject(File("part_data.json").readText())) }
 			} catch (e: Exception) {
-				withContext(Dispatchers.Main) {
-					Toast.makeText(mainActivity, e.message, Toast.LENGTH_LONG).show()
+				withContext(Main) {
+					makeText(mainActivity, e.message, LENGTH_LONG).show()
 				}
 			}
 		}
 	}
 
-	fun onPreExecute() {
+	private fun onPreExecute() {
 		if (mainActivity.checkSelfPermission(INTERNET) != PERMISSION_GRANTED) mainActivity.requestPermissions(
 			arrayOf(INTERNET),
 			0
 		)
-	}
-
-	fun doInBackground(vararg strings: Void): String {
-		val x = Scanner(
-			URL(
-				"https://firebasestorage.googleapis.com/v0/b/buildmypc-ac8c3.appspot.com/o/part_data.json?alt=media&token=" + mainActivity.getString(
-					firebase_key
-				)
-			).openConnection().getInputStream()
-		)
-		var y = ""
-		while (x.hasNextLine()) y += x.nextLine()
-		return y
-	}
-
-	fun onPostExecute(s: String) {
-		try {
-			parts.set(JSONObject(s))
-		} catch (e: JSONException) {
-			e.printStackTrace()
-		}
 	}
 }
