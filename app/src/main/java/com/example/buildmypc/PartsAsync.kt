65d3c7abd4fd10f0abceb0e3298347cfb2baa736
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-06-14 22:16:04 -0400

    Update PartsAsync.kt

--- app/src/main/java/com/example/buildmypc/PartsAsync.kt
+++ app/src/main/java/com/example/buildmypc/PartsAsync.kt
@@ -4,33 +4,15 @@ package com.example.buildmypc
 
 import android.Manifest.permission.INTERNET
 import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.net.Uri.fromFile
-import android.widget.Toast.LENGTH_LONG
-import android.widget.Toast.makeText
-import com.example.buildmypc.MainActivity.parts
-import com.example.buildmypc.MainActivity.storageReference
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
-import org.json.JSONObject
-import java.io.File
-import java.io.File.createTempFile
 
 class PartsAsync(private val mainActivity: MainActivity) {
 	fun run() {
 		CoroutineScope(IO).launch {
 			onPreExecute()
-			try {
-				storageReference.get().child("part_data.json")
-					.getFile(fromFile(withContext(IO) { createTempFile("part_data", ".json") }))
-					.await()
-				withContext(Main) { parts.set(JSONObject(File("part_data.json").readText())) }
-			} catch (e: Exception) {
-				withContext(Main) { makeText(mainActivity, e.message, LENGTH_LONG).show() }
-			}
+
 		}
 	}
 
