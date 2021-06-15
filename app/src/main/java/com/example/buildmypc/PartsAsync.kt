c6fd3651485d54d3b280d276185706875c71cd90
alpiemoodles <alktatstudios@gmail.com>
2021-06-14 20:24:32 -0400

    OHMYGOD MASSIVE PUSH - finished ALL OF NewsfeedFragment's functionality - finished MOST OF RSSAsyncTask.java excluding Engadget articles     - wrote support for Verge, WIRED, and NYTimes articles by creating custom parsers for each     - chained together every single instance of RSSAsyncTask since I can only update the UI thread once before it glitches out     - added arguments to new RSSAsyncTask() to allow for the transfer of drawables between NewsfeedFragment and instances of RSSAsyncTask - removed the button from NewsfeedFragment and made the program automatically parse the newsfeeds if finalArticleList is either null or has no entries - used the interface Article.OnDataSendToActivity (as pulled handsomely from StackOverflow) to update the main UI thread
    TODO
    - eat dinner
    - parse Engadget articles
    - (maybe) attempt to fix the faulty Firebase implementation

--- app/src/main/java/com/example/buildmypc/PartsAsync.kt
+++ app/src/main/java/com/example/buildmypc/PartsAsync.kt
@@ -4,7 +4,7 @@ package com.example.buildmypc
 
 import android.Manifest.permission.INTERNET
 import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.net.Uri.fromFile
+import android.util.Log
 import android.widget.Toast.LENGTH_LONG
 import android.widget.Toast.makeText
 import com.example.buildmypc.MainActivity.parts
@@ -13,20 +13,19 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
 import java.io.File
-import java.io.File.createTempFile
 
 class PartsAsync(private val mainActivity: MainActivity) {
 	fun run() {
 		CoroutineScope(IO).launch {
 			onPreExecute()
+
 			try {
 				storageReference.get().child("part_data.json")
-					.getFile(fromFile(withContext(IO) { createTempFile("part_data", ".json") }))
-					.await()
+					.getFile(File("part_data.json"))
+				Log.wtf("WTF","WTAF")
 				withContext(Main) { parts.set(JSONObject(File("part_data.json").readText())) }
 			} catch (e: Exception) {
 				withContext(Main) { makeText(mainActivity, e.message, LENGTH_LONG).show() }
